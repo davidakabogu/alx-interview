460bef4328935374a012c04eaf61e57e0dfce9be
DavidAkabogu <Akabogudavid@gmail.com>
2023-12-01 00:17:53 +0100

    refactor

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,20 +6,15 @@ representing the Pascal’s triangle of n
 
 
 def pascal_triangle(n):
-
     if n <= 0:
         return []
-    pascal_triangle = [0] * n
+
+    triangle = []
 
     for i in range(n):
-        # define a row and fill first and last idx with 1
-        new_row = [0] * (i + 1)
-        new_row[0] = 1
-        new_row[len(new_row) - 1] = 1
+        row = [1] * (i + 1)
         for j in range(1, i):
-            if j > 0 and j < len(new_row):
-                a = pascal_triangle[i - 1][j]
-                b = pascal_triangle[i - 1][j - 1]
-                new_row[j] = a + b
-        pascal_triangle[i] = new_row
-    return pascal_triangle
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle

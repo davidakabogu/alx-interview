90ea42e4eeaf08fed890afb9e62be03620bedecd
DavidAkabogu <Akabogudavid@gmail.com>
2024-02-12 06:34:36 +0100

    testing

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-'''Prime Game'''
+"""Prime Game"""
 
 
 def isWinner(x, nums):
-    '''finds the winner'''
-    winnerCounter = {'Maria': 0, 'Ben': 0}
+    """finds the winner"""
+    winnerCounter = {"Maria": 0, "Ben": 0}
 
     for i in range(x):
         roundWinner = isRoundWinner(nums[i], x)
         if roundWinner is not None:
             winnerCounter[roundWinner] += 1
 
-    if winnerCounter['Maria'] > winnerCounter['Ben']:
-        return 'Maria'
-    elif winnerCounter['Ben'] > winnerCounter['Maria']:
-        return 'Ben'
+    if winnerCounter["Maria"] > winnerCounter["Ben"]:
+        return "Maria"
+    elif winnerCounter["Ben"] > winnerCounter["Maria"]:
+        return "Ben"
     else:
         return None
 
 
 def isRoundWinner(n, x):
-    '''find round winner'''
+    """find round winner"""
     list = [i for i in range(1, n + 1)]
-    players = ['Maria', 'Ben']
+    players = ["Maria", "Ben"]
 
     for i in range(n):
         # get current player
@@ -60,7 +60,7 @@ def isPrime(n):
         # Not prime if divisable by another number less
         # or equal to the square root of itself.
         # n**(1/2) returns square root of n
-        for i in range(3, int(n**(1/2))+1, 2):
+        for i in range(3, int(n ** (1 / 2)) + 1, 2):
             if n % i == 0:
                 return "Not prime"
         return True

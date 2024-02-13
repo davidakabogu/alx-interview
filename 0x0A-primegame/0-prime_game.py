4affcce586c492479da8461345014fc6edf9d878
DavidAkabogu <Akabogudavid@gmail.com>
2024-02-13 06:51:50 +0100

    refactor

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,18 +2,19 @@
 """Prime Game"""
 
 
-def isPrime(n):
-    # 0, 1, even numbers greater than 2 are NOT PRIME
-    if n == 1 or n == 0 or (n % 2 == 0 and n > 2):
+def is_prime(n):
+    if n <= 1:
         return False
-    else:
-        # Not prime if divisable by another number less
-        # or equal to the square root of itself.
-        # n**(1/2) returns square root of n
-        for i in range(3, int(n ** (1 / 2)) + 1, 2):
-            if n % i == 0:
-                return "Not prime"
+    if n <= 3:
         return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 
 def isRoundWinner(n, x):
